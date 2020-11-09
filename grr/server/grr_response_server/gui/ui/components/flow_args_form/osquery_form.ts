bb13b7ccfb01b5e9922ee3c9f561886cfd278ff0
Simeon Stoykov <simstoykov@google.com>
2020-11-09 18:28:35 +0000

    Added the code but not the dependencies

--- grr/server/grr_response_server/gui/ui/components/flow_args_form/osquery_form.ts
+++ grr/server/grr_response_server/gui/ui/components/flow_args_form/osquery_form.ts
@@ -1,9 +1,16 @@
-import {ChangeDetectionStrategy, Component, OnInit, Output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit, Output, Input, ViewChild, ElementRef, AfterViewInit, ViewEncapsulation} from '@angular/core';
 import {FlowArgumentForm} from '@app/components/flow_args_form/form_interface';
 
 import {OsqueryArgs} from '../../lib/api/api_interfaces';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
 import {shareReplay} from 'rxjs/operators';
+
+import * as CodeMirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/sql/sql.js';
+import 'codemirror/addon/hint/show-hint.js';
+import 'codemirror/addon/hint/sql-hint.js';
+import 'codemirror/addon/hint/anyword-hint.js';
 
 
 /** Form that configures an Osquery flow. */
@@ -12,18 +19,31 @@ import {shareReplay} from 'rxjs/operators';
   templateUrl: './osquery_form.ng.html',
   styleUrls: ['./osquery_form.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
-export class OsqueryForm extends FlowArgumentForm<OsqueryArgs> implements OnInit {
+export class OsqueryForm extends FlowArgumentForm<OsqueryArgs> implements OnInit, AfterViewInit {
   readonly form = new FormGroup({
     query: new FormControl(null, Validators.required),
     timeoutMillis: new FormControl(null, Validators.required),
     ignoreStderrErrors: new FormControl(null),
   });
 
+  @ViewChild('editor') readonly editor!: ElementRef;
+
   @Output() readonly formValues$ = this.form.valueChanges.pipe(shareReplay(1));
   @Output() readonly status$ = this.form.statusChanges.pipe(shareReplay(1));
 
   ngOnInit() {
     this.form.patchValue(this.defaultFlowArgs);
+  }
+
+  ngAfterViewInit(): void {
+    CodeMirror.fromTextArea(this.editor.nativeElement, {
+      value: '',
+      mode: 'text/x-sqlite',
+      theme: 'idea',
+      extraKeys: {'Ctrl-Space': 'autocomplete'},
+      lineNumbers: true,
+    });
   }
 }
