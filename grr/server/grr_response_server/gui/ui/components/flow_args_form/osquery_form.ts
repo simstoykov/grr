9bbb337ca2abf8d6e775cfa6565a58cdee8c3d48
Simeon Stoykov <simstoykov@google.com>
2020-11-12 19:01:28 +0000

    [WIP] Added simple templates and a 550kb tables file

--- grr/server/grr_response_server/gui/ui/components/flow_args_form/osquery_form.ts
+++ grr/server/grr_response_server/gui/ui/components/flow_args_form/osquery_form.ts
@@ -3,7 +3,7 @@ import {FlowArgumentForm} from '@app/components/flow_args_form/form_interface';
 
 import {OsqueryArgs} from '../../lib/api/api_interfaces';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
-import {shareReplay} from 'rxjs/operators';
+import {shareReplay, startWith, map, filter, debounceTime, tap} from 'rxjs/operators';
 
 import * as CodeMirror from 'codemirror';
 import 'codemirror/mode/sql/sql.js';
@@ -11,6 +11,58 @@ import 'codemirror/addon/hint/show-hint.js';
 import 'codemirror/addon/hint/sql-hint.js';
 import 'codemirror/addon/hint/anyword-hint.js';
 
+import {tables} from './temporary_list_of_osquery_tables';
+import { Observable } from 'rxjs';
+
+const INPUT_DEVOUNCE_TIME_MS = 200;
+
+interface Column {
+  name: string,
+  required: boolean,
+}
+
+interface Table {
+  name: string,
+  description: string,
+  columns: ReadonlyArray<Column>,
+  platforms: ReadonlyArray<string>,
+}
+
+function nameToTable(name: string) {
+  return tables.filter(fullInfo => fullInfo.name === name)[0];
+}
+
+function constructFromClause(table: Table): string {
+  return `FROM ${table.name}`
+}
+
+function constructSelectClause(table: Table): string {
+  const allColumnNames = table.columns.map(column => column.name);
+  const columnsWithSeparator = allColumnNames.join(',\n\t');
+  return `SELECT\n\t${columnsWithSeparator}`;
+}
+
+function constructWhereClause(table: Table): string {
+  const requiredColumnNames = table.columns
+    .filter(column => column.required)
+    .map(column => column.name);
+  if (!requiredColumnNames) {
+    return '';
+  }
+
+  const comment = 'TODO: Set required filed as appropriate.'
+  const columnConstraints = requiredColumnNames.map(colName => `${colName} LIKE "" -- ${comment}`);
+  const whereClauseArgs = columnConstraints.join('\nAND\n\t');
+
+  return `WHERE\n\t${whereClauseArgs}`;
+}
+
+function constructQueryFromTable(table: Table): string {
+  const selectClause = constructSelectClause(table);
+  const fromClause = constructFromClause(table);
+  const whereClause = constructWhereClause(table);
+  return [selectClause, fromClause, whereClause, ';'].join('\n');
+}
 
 /** Form that configures an Osquery flow. */
 @Component({
@@ -21,28 +73,64 @@ import 'codemirror/addon/hint/anyword-hint.js';
   encapsulation: ViewEncapsulation.None,
 })
 export class OsqueryForm extends FlowArgumentForm<OsqueryArgs> implements OnInit, AfterViewInit {
+  readonly templateTableControl = new FormControl(null);
+
+  readonly filteredTables$ = this.templateTableControl.valueChanges.pipe(
+    debounceTime(INPUT_DEVOUNCE_TIME_MS),
+    startWith(''),
+    map(keyword => tables.filter(table => table.name.includes(keyword))),
+  );
+
+  readonly selectedTemplateTable$: Observable<Table | undefined> = this.templateTableControl.valueChanges.pipe(
+    debounceTime(INPUT_DEVOUNCE_TIME_MS),
+    map(nameToTable),
+  );
+
   readonly form = new FormGroup({
     query: new FormControl(null, Validators.required),
     timeoutMillis: new FormControl(null, Validators.required),
     ignoreStderrErrors: new FormControl(null),
   });
 
-  @ViewChild('editorTarget') readonly editorTarget!: ElementRef;
-
   @Output() readonly formValues$ = this.form.valueChanges.pipe(shareReplay(1));
   @Output() readonly status$ = this.form.statusChanges.pipe(shareReplay(1));
+
+  @ViewChild('editorTarget')
+  readonly editorTarget!: ElementRef;
+  private editor?: CodeMirror.Editor;
 
   ngOnInit() {
     this.form.patchValue(this.defaultFlowArgs);
   }
 
   ngAfterViewInit(): void {
-    CodeMirror.fromTextArea(this.editorTarget.nativeElement, {
+    this.editor = this.initializeEditor();
+  }
+
+  onSubmit(selectedTable: Table): void {
+    const query = constructQueryFromTable(selectedTable);
+    this.editor?.setValue(query);
+  }
+
+  trackByTableName(_: number, table: Table): string {
+    return table.name;
+  }
+
+  private initializeEditor(): CodeMirror.Editor {
+    const editor = CodeMirror.fromTextArea(this.editorTarget.nativeElement, {
       value: '',
       mode: 'text/x-sqlite',
       theme: 'idea',
       extraKeys: {'Ctrl-Space': 'autocomplete'},
       lineNumbers: true,
     });
+
+    editor.on('change', () => {
+      this.form.patchValue({
+        query: editor.getValue(),
+      });
+    });
+
+    return editor;
   }
 }
