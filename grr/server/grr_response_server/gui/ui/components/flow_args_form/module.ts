828868832d96d1962120fa45d1ed083423776ef6
Simeon Stoykov <simstoykov@google.com>
2020-11-12 19:01:28 +0000

    [WIP] Added simple templates and a 550kb tables file

--- grr/server/grr_response_server/gui/ui/components/flow_args_form/module.ts
+++ grr/server/grr_response_server/gui/ui/components/flow_args_form/module.ts
@@ -1,20 +1,28 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+
 import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+
 import {MatButtonModule} from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {RouterModule} from '@angular/router';
-import {CollectBrowserHistoryForm} from '@app/components/flow_args_form/collect_browser_history_form';
-import {CollectMultipleFilesForm} from '@app/components/flow_args_form/collect_multiple_files_form';
-import {CollectSingleFileForm} from '@app/components/flow_args_form/collect_single_file_form';
+
 import {ByteComponentsModule} from '@app/components/form/byte_input/module';
 import {GlobExpressionExplanationModule} from '@app/components/form/glob_expression_form_field/module';
 
 import {FallbackFlowArgsForm} from './fallback_flow_args_form';
+
+import {CollectBrowserHistoryForm} from '@app/components/flow_args_form/collect_browser_history_form';
+import {CollectMultipleFilesForm} from '@app/components/flow_args_form/collect_multiple_files_form';
+import {CollectSingleFileForm} from '@app/components/flow_args_form/collect_single_file_form';
 import {FlowArgsForm} from './flow_args_form';
 import {OsqueryForm} from './osquery_form';
 
@@ -24,12 +32,15 @@ import {OsqueryForm} from './osquery_form';
     BrowserAnimationsModule,
     RouterModule,
     CommonModule,
-    MatCheckboxModule,
     ReactiveFormsModule,
+    FormsModule,
+    MatCheckboxModule,
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
     MatButtonModule,
+    MatAutocompleteModule,
+    MatTooltipModule,
     ByteComponentsModule,
     GlobExpressionExplanationModule,
   ],
