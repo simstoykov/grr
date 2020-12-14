349187397718278436ecd6519b2ea33939f8e620
Simeon Stoykov <simstoykov@google.com>
2020-12-14 11:32:52 +0000

    Renamed protobufs to be backward compatible; simplified tests; raising column exception on flow start; other minor changes

--- grr/server/grr_response_server/gui/ui/components/flow_args_form/osquery_form.ts
+++ grr/server/grr_response_server/gui/ui/components/flow_args_form/osquery_form.ts
@@ -4,7 +4,7 @@ import {FlowArgumentForm} from '@app/components/flow_args_form/form_interface';
 import {shareReplay} from 'rxjs/operators';
 import {MatDialog} from '@angular/material/dialog';
 
-import {OsqueryArgs} from '../../lib/api/api_interfaces';
+import {OsqueryFlowArgs} from '../../lib/api/api_interfaces';
 
 
 /** Form that configures an Osquery flow. */
@@ -14,7 +14,7 @@ import {OsqueryArgs} from '../../lib/api/api_interfaces';
   styleUrls: ['./osquery_form.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OsqueryForm extends FlowArgumentForm<OsqueryArgs> implements
+export class OsqueryForm extends FlowArgumentForm<OsqueryFlowArgs> implements
     OnInit {
   private readonly defaultQueryDisplayed = 'SELECT * FROM users LIMIT 10;';
 
